5709c2cf75d044f0908e4590ed00c2f5d0d59a47
sgern <sgern@users.noreply.github.com>
2023-03-09 15:12:11 -0500

    Create Card class

---
+++ src/main/java/com/sgern/TitleBoutII/Card.java
@@ -0,0 +1,132 @@
+package com.sgern.TitleBoutII;
+
+public class Card {
+	
+	private int cf;
+	private int rn;
+	private int result;
+	private boolean cut;
+	private String ringPosition;
+	private int kd;
+	private int kd2;
+	private int kor;
+	private int kdc;
+	private int j1;
+	private int j2;
+	private int j3;
+	private int injury;
+	private int specialAction;
+	
+	public int getCF() {
+		return cf;
+	}
+	
+	public void setCF(int cf) {
+		this.cf = cf;
+	}
+	
+	public int getRN() {
+		return rn;
+	}
+	
+	public void setRN(int rn) {
+		this.rn = rn;
+	}
+	
+	public int getResult() {
+		return result;
+	}
+	
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+	public boolean isCut() {
+		return cut;
+	}
+
+	public void setCut(boolean cut) {
+		this.cut = cut;
+	}
+
+	public String getRingPosition() {
+		return ringPosition;
+	}
+
+	public void setRingPosition(String ringPosition) {
+		this.ringPosition = ringPosition;
+	}
+
+	public int getKD() {
+		return kd;
+	}
+
+	public void setKD(int kd) {
+		this.kd = kd;
+	}
+
+	public int getKD2() {
+		return kd2;
+	}
+
+	public void setKD2(int kd2) {
+		this.kd2 = kd2;
+	}
+
+	public int getKOR() {
+		return kor;
+	}
+
+	public void setKOR(int kor) {
+		this.kor = kor;
+	}
+
+	public int getKDC() {
+		return kdc;
+	}
+
+	public void setKDC(int kdc) {
+		this.kdc = kdc;
+	}
+
+	public int getJ1() {
+		return j1;
+	}
+
+	public void setJ1(int j1) {
+		this.j1 = j1;
+	}
+
+	public int getJ2() {
+		return j2;
+	}
+
+	public void setJ2(int j2) {
+		this.j2 = j2;
+	}
+
+	public int getJ3() {
+		return j3;
+	}
+
+	public void setJ3(int j3) {
+		this.j3 = j3;
+	}
+
+	public int getInjury() {
+		return injury;
+	}
+
+	public void setInjury(int injury) {
+		this.injury = injury;
+	}
+
+	public int getSpecialAction() {
+		return specialAction;
+	}
+
+	public void setSpecialAction(int specialAction) {
+		this.specialAction = specialAction;
+	}
+	
+}
