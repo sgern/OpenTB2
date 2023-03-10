6dd9aa1cab2f32b6ee56a7a0364500b9036af7dd
sgern <sgern@users.noreply.github.com>
2023-03-10 10:33:52 -0500

    Create CornerMan class

---
+++ src/main/java/com/sgern/TitleBoutII/CornerMan.java
@@ -0,0 +1,69 @@
+package com.sgern.TitleBoutII;
+
+public class CornerMan {
+	
+	private String name;
+	private String training;
+	private String strategy;
+	private String attitude;
+	private String cutsSwell;
+	private int boxerRate;
+	private int availability;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getTraining() {
+		return training;
+	}
+	
+	public void setTraining(String training) {
+		this.training = training;
+	}
+	
+	public String getStrategy() {
+		return strategy;
+	}
+	
+	public void setStrategy(String strategy) {
+		this.strategy = strategy;
+	}
+	
+	public String getAttitude() {
+		return attitude;
+	}
+	
+	public void setAttitude(String attitude) {
+		this.attitude = attitude;
+	}
+	
+	public String getCutsSwell() {
+		return cutsSwell;
+	}
+	
+	public void setCutsSwell(String cutsSwell) {
+		this.cutsSwell = cutsSwell;
+	}
+	
+	public int getBoxerRate() {
+		return boxerRate;
+	}
+	
+	public void setBoxerRate(int boxerRate) {
+		this.boxerRate = boxerRate;
+	}
+	
+	public int getAvailability() {
+		return availability;
+	}
+	
+	public void setAvailability(int availability) {
+		this.availability = availability;
+	}
+	
+}
