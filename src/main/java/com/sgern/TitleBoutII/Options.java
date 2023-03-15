f5a4b392c5a2f7adfa8a30280597896090f3da05
sgern <sgern@users.noreply.github.com>
2023-03-15 15:50:15 -0400

    Create Options class

---
+++ src/main/java/com/sgern/TitleBoutII/Options.java
@@ -0,0 +1,187 @@
+package com.sgern.TitleBoutII;
+
+public class Options {
+	
+	private int maxRounds;
+	private boolean cfConversionEnabled;
+	private boolean aggressivenessEnabled;
+	private boolean foulDamageEnabled;
+	private boolean headbuttCutEnabled;
+	private String advancedTimingOption;
+	private boolean extendRoundEnabled;
+	private boolean mandatoryEightCountEnabled;
+	private boolean savedByTheBellEnabled;
+	private boolean threeKnockdownRuleEnabled;
+	private boolean southpawEnabled;
+	private boolean missPenaltyEnabled;
+	private boolean kiTimingEnabled;
+	private boolean advancedClinchingEnabled;
+	private boolean ratedRefereeEnabled;
+	private boolean ratedCornerMenEnabled;
+	private String refereeErrorOption;
+	private boolean strategyEnabled;
+	private boolean fighterTraitsEnabled;
+	private boolean throwInTheTowelEnabled;
+	
+	public int getMaxRounds() {
+		return maxRounds;
+	}
+
+	public void setMaxRounds(int maxRounds) {
+		this.maxRounds = maxRounds;
+	}
+
+	public boolean isCFConversionEnabled() {
+		return cfConversionEnabled;
+	}
+	
+	public void setCFConversionEnabled(boolean cfConversionEnabled) {
+		this.cfConversionEnabled = cfConversionEnabled;
+	}
+
+	public boolean isAggressivenessEnabled() {
+		return aggressivenessEnabled;
+	}
+
+	public void setAggressivenessEnabled(boolean aggressivenessEnabled) {
+		this.aggressivenessEnabled = aggressivenessEnabled;
+	}
+
+	public boolean isFoulDamageEnabled() {
+		return foulDamageEnabled;
+	}
+
+	public void setFoulDamageEnabled(boolean foulDamageEnabled) {
+		this.foulDamageEnabled = foulDamageEnabled;
+	}
+
+	public boolean isHeadbuttCutEnabled() {
+		return headbuttCutEnabled;
+	}
+
+	public void setHeadbuttCutEnabled(boolean headbuttCutEnabled) {
+		this.headbuttCutEnabled = headbuttCutEnabled;
+	}
+
+	public String getAdvancedTimingOption() {
+		return advancedTimingOption;
+	}
+
+	public void setAdvancedTimingOption(String advancedTimingOption) {
+		this.advancedTimingOption = advancedTimingOption;
+	}
+
+	public boolean isExtendRoundEnabled() {
+		return extendRoundEnabled;
+	}
+
+	public void setExtendRoundEnabled(boolean extendRoundEnabled) {
+		this.extendRoundEnabled = extendRoundEnabled;
+	}
+
+	public boolean isMandatoryEightCountEnabled() {
+		return mandatoryEightCountEnabled;
+	}
+
+	public void setMandatoryEightCountEnabled(boolean mandatoryEightCountEnabled) {
+		this.mandatoryEightCountEnabled = mandatoryEightCountEnabled;
+	}
+
+	public boolean isSavedByTheBellEnabled() {
+		return savedByTheBellEnabled;
+	}
+
+	public void setSavedByTheBellEnabled(boolean savedByTheBellEnabled) {
+		this.savedByTheBellEnabled = savedByTheBellEnabled;
+	}
+
+	public boolean isThreeKnockdownRuleEnabled() {
+		return threeKnockdownRuleEnabled;
+	}
+
+	public void setThreeKnockdownRuleEnabled(boolean threeKnockdownRuleEnabled) {
+		this.threeKnockdownRuleEnabled = threeKnockdownRuleEnabled;
+	}
+
+	public boolean isSouthpawEnabled() {
+		return southpawEnabled;
+	}
+
+	public void setSouthpawEnabled(boolean southpawEnabled) {
+		this.southpawEnabled = southpawEnabled;
+	}
+
+	public boolean isMissPenaltyEnabled() {
+		return missPenaltyEnabled;
+	}
+
+	public void setMissPenaltyEnabled(boolean missPenaltyEnabled) {
+		this.missPenaltyEnabled = missPenaltyEnabled;
+	}
+
+	public boolean isKITimingEnabled() {
+		return kiTimingEnabled;
+	}
+
+	public void setKITimingEnabled(boolean kiTimingEnabled) {
+		this.kiTimingEnabled = kiTimingEnabled;
+	}
+
+	public boolean isAdvancedClinchingEnabled() {
+		return advancedClinchingEnabled;
+	}
+
+	public void setAdvancedClinchingEnabled(boolean advancedClinchingEnabled) {
+		this.advancedClinchingEnabled = advancedClinchingEnabled;
+	}
+
+	public boolean isRatedRefereeEnabled() {
+		return ratedRefereeEnabled;
+	}
+
+	public void setRatedRefereeEnabled(boolean ratedRefereeEnabled) {
+		this.ratedRefereeEnabled = ratedRefereeEnabled;
+	}
+
+	public boolean isRatedCornerMenEnabled() {
+		return ratedCornerMenEnabled;
+	}
+
+	public void setRatedCornerMenEnabled(boolean ratedCornerMenEnabled) {
+		this.ratedCornerMenEnabled = ratedCornerMenEnabled;
+	}
+
+	public String getRefereeErrorOption() {
+		return refereeErrorOption;
+	}
+
+	public void setRefereeErrorOption(String refereeErrorOption) {
+		this.refereeErrorOption = refereeErrorOption;
+	}
+
+	public boolean isStrategyEnabled() {
+		return strategyEnabled;
+	}
+
+	public void setStrategyEnabled(boolean strategyEnabled) {
+		this.strategyEnabled = strategyEnabled;
+	}
+
+	public boolean isFighterTraitsEnabled() {
+		return fighterTraitsEnabled;
+	}
+
+	public void setFighterTraitsEnabled(boolean fighterTraitsEnabled) {
+		this.fighterTraitsEnabled = fighterTraitsEnabled;
+	}
+
+	public boolean isThrowInTheTowelEnabled() {
+		return throwInTheTowelEnabled;
+	}
+
+	public void setThrowInTheTowelEnabled(boolean throwInTheTowelEnabled) {
+		this.throwInTheTowelEnabled = throwInTheTowelEnabled;
+	}
+	
+	
+}
