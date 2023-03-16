7cfb466b95f703e0248f5b67f79d18c2bf24326b
sgern <sgern@users.noreply.github.com>
2023-03-16 11:29:14 -0400

    Create Deck class

---
+++ src/main/java/com/sgern/TitleBoutII/Deck.java
@@ -0,0 +1,42 @@
+package com.sgern.TitleBoutII;
+
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+	
+	private List<Card> drawPile;
+	private List<Card> discardPile;
+	
+	public Card drawCard() {
+		Card card = drawPile.remove(0);
+		discardPile.add(card);
+		return card;
+	}
+	
+	public Card drawAndReturn() {
+		Card card = drawPile.remove(0);
+		drawPile.add(card);
+		return card;
+	}
+	
+	public List<Card> drawStack(int cards) {
+		List<Card> stack = new ArrayList<>();
+		for (int i = 0; i < cards && !this.isEmpty(); i++) {
+			stack.add(this.drawCard());
+		}
+		return stack;
+	}
+	
+	public void shuffleDeck() {
+		drawPile.addAll(discardPile);
+		discardPile.clear();
+		Collections.shuffle(drawPile);
+	}
+	
+	public boolean isEmpty() {
+		return drawPile.isEmpty();
+	}
+	
+}
